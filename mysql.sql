2f943a673469a7afd575b88c5d23ec4dadfdb542
Shubham <chaudhary.shubham812@gmail.com>
2024-06-17 23:12:32 +0100

    added query

--- mysql.sql
+++ mysql.sql
@@ -14,3 +14,16 @@ from df_orders
 group by region, product_id
 order by region ,sales Desc;
 
+
+/* write */
+/***/
+select region, sum(sale_price) as sales
+from df_orders
+group by region, product_id
+order by region ,sales Desc;
+
+/* top 5 highest selling products in each region*/
+select region, product_id, sum(sale_price) as sales
+from df_orders
+group by region, product_id
+order by region ,sales Desc;
